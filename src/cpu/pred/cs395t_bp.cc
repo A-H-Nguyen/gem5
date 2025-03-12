6015dacb9d2070b26f88c31483de7f2ef14003ee
Andrew Nguyen <andrewnguyen@utexas.edu>
2025-03-12 16:13:59 -0500

    Added RCR to CS395T bp class

--- src/cpu/pred/cs395t_bp.cc
+++ src/cpu/pred/cs395t_bp.cc
@@ -20,45 +20,173 @@ namespace gem5
 namespace branch_prediction
 {
 
-CS395TBP::CS395TBP(const CS395TBPParams &params)
-    : BPredUnit(params), example_size(params.size)
-    // TODO: Add more params here
+// bool CS395TBP::predict(ThreadID tid, Addr pc, bool cond_branch, void* &b) {
+//   return false;
+// }
+
+// void
+// CS395TBP::update(ThreadID tid, Addr pc, bool taken, void * &bp_history, 
+//                  bool squashed, const StaticInstPtr & inst, Addr target)
+// {
+//     // TODO FIXME: Main update function when ground truth for the branch is
+//     // computed.
+// }
+
+// void
+// CS395TBP::updateHistories(ThreadID tid, Addr branch_pc, bool taken, 
+//                          TAGEBase::BranchInfo* b, bool speculative, 
+//                          const StaticInstPtr &inst, Addr target)
+// {
+//     // TODO FIXME: Update the branch predictor's internal structures
+//     // (e.g. path, global history) after the branch was resolved. It'll get
+//     // called automatically by gem5.
+//     //
+//     // Note that this also gets called for unconditional branches, so you may
+//     // choose to filter them out if necessary.
+// }
+
+// void
+// CS395TBP::squash(ThreadID tid, bool taken, TAGEBase::BranchInfo *bi,
+//                  Addr target)
+// {
+//     // TODO FIXME: Clean up branch state when there is a pipeline squash.
+//     // If bp_history is being used, then this is likely where to free the
+//     // memory.  Also, update any speculative state.
+// }
+
+// bool
+// CS395TBP::lookup(ThreadID tid, Addr branch_addr, void * &bp_history)
+// {
+//     // TODO FIXME: Make the prediction for the branch.
+//     return false;
+// }
+
+
+/************************************************************
+ * RCR Functionality
+ */
+
+ RCR::RCR(int _T, int _W, int _D, int _shift, int _CTWidth)
+ : CTWidth(_CTWidth), T(_T), W(_W), D(_D), S(_shift)
 {
+  bb[0].resize(maxwindow);
+  bb[1].resize(maxwindow);
+  ctxs = {0, 0};
+  // printf("\n\nRCR: context hash config: [T:%i, W:%i, D:%i, S:%i, CTWidth:%i]\n",
+  //         T, W, D, S, CTWidth);
 }
 
-bool
-CS395TBP::lookup(ThreadID tid, Addr branch_addr, void * &bp_history)
+
+/*
+* Given the {n} number of branches staring from vec[end-start]
+* to vec[end-start-n-1] we create the hash function by shifting
+* each PC by {shift} number if bits i.e.
+*
+*   000000000000|  PC  |    :vec[end-start]
+* ^ 0000000000|  PC  |00    :vec[end-start-1]
+* ^ 00000000|  PC  |0000    :vec[end-start-2]
+*           .                     .
+*           .                     .
+*           .                     .
+* ^ |  PC  |000000000000    :vec[end-start-n-1]
+* ----------------------
+*       final hash value
+* */
+unsigned long
+RCR::calcHash(std::list<unsigned long> &vec, int n, int start, int shift)
 {
-    // TODO FIXME: Make the prediction for the branch.
-    return false;
+  unsigned long hash = 0;
+  if (vec.size() < (start + n)) {
+      return 0;
+  }
+  unsigned long sh = 0;
+  auto it = vec.begin();
+  std::advance(it, start);
+  for (; (it != vec.end()) && (n > 0); it++, n--) {
+    unsigned long val = *it;
+
+    // Shift the value
+    hash ^= val << (unsigned long)(sh);
+
+    sh += shift;
+    if (sh >= CTWidth) {
+      sh -= (unsigned long)(CTWidth);
+    }
+  }
+  return hash & ((1 << CTWidth) - 1);
 }
 
-void
-CS395TBP::updateHistories(ThreadID tid, Addr pc, bool uncond,
-                          bool taken, Addr target, void * &bp_history)
+unsigned long RCR::getCCID()
 {
-    // TODO FIXME: Update the branch predictor's internal structures
-    // (e.g. path, global history) after the branch was resolved. It'll get
-    // called automatically by gem5.
-    //
-    // Note that this also gets called for unconditional branches, so you may
-    // choose to filter them out if necessary.
+  return ctxs.ccid & ((1 << CTWidth) - 1);
 }
 
-void
-CS395TBP::squash(ThreadID tid, void * &bp_history)
+unsigned long RCR::getPCID() 
 {
-    // TODO FIXME: Clean up branch state when there is a pipeline squash.
-    // If bp_history is being used, then this is likely where to free the
-    // memory.  Also, update any speculative state.
+  return ctxs.pcid & ((1 << CTWidth) - 1);
 }
 
-void
-CS395TBP::update(ThreadID tid, Addr pc, bool taken, void * &bp_history,
-                 bool squashed, const StaticInstPtr &inst, Addr target)
+
+bool RCR::update(unsigned long pc, OpType opType, bool taken) 
 {
-    // TODO FIXME: Main update function when ground truth for the branch is
-    // computed.
+  branchCount++;
+  // Hash of all branches
+  auto isCall = (opType == OPTYPE_CALL_DIRECT_UNCOND)
+        || (opType == OPTYPE_CALL_INDIRECT_UNCOND)
+        || (opType == OPTYPE_CALL_DIRECT_COND);
+
+
+  switch (T) {
+    case 0: // All branches
+    bb[0].push_front(pc);
+    bb[1].push_front(branchCount);
+    break;
+
+    case 1: // Only calls
+    if (isCall) {
+        bb[0].push_front(pc);
+        bb[1].push_front(branchCount);
+    }
+    break;
+
+    case 2: // Only calls and returns
+    if (isCall || (opType == OPTYPE_RET_UNCOND)) {
+        bb[0].push_front(pc);
+        bb[1].push_front(branchCount);
+    }
+    break;
+
+    case 3: // Only unconditional branches
+    if (opType != OPTYPE_JMP_DIRECT_COND) {
+        bb[0].push_front(pc);
+        bb[1].push_front(branchCount);
+    }
+    break;
+
+    case 4: // All taken branches
+    if (taken) {
+        bb[0].push_front(pc);
+        bb[1].push_front(branchCount);
+    }
+    break;
+  }
+
+// PRINTIF(COND,"UH:%llx, %i, %i\n", pc, opType, taken);
+  // If the size has changed the hash has changed
+  bool changed = false;
+  if (bb[0].size() > maxwindow) {
+      changed = true;
+
+      // Resize the history
+      bb[0].pop_back();
+      bb[1].pop_back();
+
+      // The current context.
+      ctxs.ccid = calcHash(bb[0], W, D, S);
+      // The prefetch context.
+      ctxs.pcid = calcHash(bb[0], W, 0, S);
+  }
+  return changed;
 }
 
 } // namespace branch_prediction
--- src/cpu/pred/cs395t_bp.hh
+++ src/cpu/pred/cs395t_bp.hh
@@ -2,7 +2,13 @@
 #define __CPU_PRED_CS395T_BP_PRED_HH__
 
 #include "base/sat_counter.hh"
-#include "cpu/pred/bpred_unit.hh"
+// #include "cpu/pred/bpred_unit.hh"
+#include "cpu/pred/tage_sc_l.hh"
+#include "cpu/pred/tage_sc_l_64KB.hh"
+#include "cpu/pred/statistical_corrector.hh"
+#include "params/TAGE_SC_L.hh"
+#include "params/TAGE_SC_L_LoopPredictor.hh"
+#include "params/TAGE_SC_L_TAGE.hh"
 #include "params/CS395TBP.hh"
 
 namespace gem5
@@ -13,20 +19,129 @@ struct CS395TBPParams;
 namespace branch_prediction
 {
 
-class CS395TBP : public BPredUnit
+//JD2_17_2016 break down types into COND/UNCOND
+typedef enum {
+  OPTYPE_OP               =2,
+
+  OPTYPE_RET_UNCOND,
+  OPTYPE_JMP_DIRECT_UNCOND,
+  OPTYPE_JMP_INDIRECT_UNCOND,
+  OPTYPE_CALL_DIRECT_UNCOND,
+  OPTYPE_CALL_INDIRECT_UNCOND,
+
+  OPTYPE_RET_COND,
+  OPTYPE_JMP_DIRECT_COND,
+  OPTYPE_JMP_INDIRECT_COND,
+  OPTYPE_CALL_DIRECT_COND,
+  OPTYPE_CALL_INDIRECT_COND,
+
+  OPTYPE_ERROR,
+
+  OPTYPE_MAX
+} OpType;
+
+typedef enum {
+    NoBranch,
+    Return,
+    CallDirect,
+    CallIndirect,
+    DirectCond,
+    DirectUncond,
+    IndirectCond,
+    IndirectUncond,
+    MAX
+} BrType;
+
+/********************************************************************
+ * Rolling Context Register RCR
+ *
+ * The RCR maintains the previous executed branches to compute
+ * a context ID.
+ *
+ * The hash function is defined by 4 paramenters
+ *
+ * T: Type of history (T). Which branches should be hased
+ *     0: All branches, 1: Only calls, 2: Calls and returns
+ *     3: All unconditional branches, 4: All taken branches
+ *
+ * W: Number of branches that should be hashed (W in the paper).
+ * D: Number of most recent branches skipped for CCID. Adds delay which
+ *    is used to prefetch. (D in the paper.)
+ * S: Number of bits to shift the PC's. Is useful to avoid ping-pong context
+ *    due to the XOR function in case a loop is executed
+ *
+ * ********************************************************************* *
+ * EXAMPLE                                       
+ *                       pb-index (2.)  (3.)                             *
+ *                      v             v     v                            *
+ * history buffer : |l|k|j|i|h|g|f|e|d|c|b|a|                            *
+ *                            ^prefetch (2.)^                            *
+ * a is the newest branch PC added to the buffer, l the oldest.          *
+ * (2.) = W = 7; (3.) = D = 3                                            *
+ * branches used to obtain PB index hash: j to d                         *
+ * branches used to obtain hash to prefetch into PB: g to a  
+ * *********************************************************************
+ */
+class RCR {
+  const int maxwindow = 120;
+
+  unsigned long 
+  calcHash(std::list<unsigned long> &vec, int n, int start=0, int shift=0);
+
+  // The context tag width
+  const int CTWidth;
+
+  // A list of previouly taken branches
+  std::list<unsigned long> bb[10];
+
+  // We compute the context ID and prefetch context ID
+  // only when the content of the RCR changes.
+  struct {
+      unsigned long ccid = 0;
+      unsigned long pcid = 0;
+  } ctxs;
+
+  int branchCount = 0;
+
+public:
+    // The hash constants
+    const int T, W, D, S;
+
+    RCR(int _T, int _W, int _D, int _shift, int _CTWidth);
+
+    // Push a new branch into the RCR.
+    bool update(unsigned long pc, OpType type, bool taken);
+
+    // Get the current context ID
+    unsigned long getCCID();
+
+    // Get the prefetch context ID
+    unsigned long getPCID();
+};
+
+// class CS395TBP : public BPredUnit
+class CS395TBP : public TAGE_SC_L_TAGE
 {
   protected:
     const unsigned example_size; // Example parameter.
     // TODO: Add more params here
 
   public:
-    CS395TBP(const CS395TBPParams &params);
-    bool lookup(ThreadID tid, Addr pc, void * &bp_history) override;
-    void updateHistories(ThreadID tid, Addr pc, bool uncond,
-                         bool taken, Addr target, void * &bp_history) override;
-    void squash(ThreadID tid, void * &bp_history) override;
-    void update(ThreadID tid, Addr pc, bool taken, void * &bp_history,
-                bool squashed, const StaticInstPtr &inst, Addr target) override;
+    CS395TBP(const CS395TBPParams &params) : TAGE_SC_L_TAGE(p)
+    {}
+
+    // bool predict(ThreadID tid, Addr pc, bool cond_branch, void* &b) override;
+    // void update(ThreadID tid, Addr pc, bool taken,
+    //             void * &bp_history, bool squashed,
+    //             const StaticInstPtr & inst, Addr target) override;
+    // void updateHistories(
+    //     ThreadID tid, Addr branch_pc, bool taken, TAGEBase::BranchInfo* b,
+    //     bool speculative, const StaticInstPtr &inst,
+    //     Addr target) override;
+    // void squash(ThreadID tid, bool taken, TAGEBase::BranchInfo *bi,
+    //     Addr target) override;
+
+    // bool lookup(ThreadID tid, Addr pc, void * &bp_history);
 };
 
 } // namespace branch_prediction
